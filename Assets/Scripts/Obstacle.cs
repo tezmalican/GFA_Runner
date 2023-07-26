d978c1b93daf4a6ac047effcd060f53bae70e511
Mehmet Ali Cantez <mehmetalicantez@gmail.com>
2023-07-26 20:34:12 +0300

    Project Updates

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _forwardSpeed;
+
+    [SerializeField] private float _horizontalSpeed;
+
+    [SerializeField] private Rigidbody _rigidbody;
+
+    private Vector3 _velocity = new Vector3();
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        _velocity.z = _forwardSpeed;
+        _velocity.z = _rigidbody.velocity.y;
+        _velocity.x = Input.GetAxis("Horizontal") * _horizontalSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = _velocity;
+    }
+}
