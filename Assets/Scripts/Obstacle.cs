3298d0a7b040c714afc8c83d5775999496060ba7
Mehmet Ali Cantez <mehmetalicantez@gmail.com>
2023-07-26 20:34:33 +0300

    Update Obstacle.cs

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,7 +4,25 @@ public class Obstacle : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-
+        if (collision.rigidbody.CompareTag("Player"))
+        {
+            Debug.Log("CollisionEnter: " + collision.rigidbody.name);
+        }
     }
 
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.rigidbody.CompareTag("Player"))
+        {
+            Debug.Log("CollisionExit: " + collision.rigidbody.name);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.rigidbody.CompareTag("Player"))
+        {
+            Debug.Log("CollisionStay: " + collision.rigidbody.name);
+        }
+    }
 }
